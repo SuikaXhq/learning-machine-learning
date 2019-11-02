e6ccf47712c82bca2af67abe899d98d1d64d7cdd
SuikaXhq <xuhaoqing2013@hotmail.com>
2019-11-02 16:04:53 +0800

    modded hello_gd

--- source/hello_gd.py
+++ source/hello_gd.py
@@ -14,11 +14,11 @@ if __name__ == '__main__':
     plt.savefig('origin.png')
 
     # Train
-    
-    # a is the parameter
+
+    # model: y = a[0] + a[1] * x
+    # a is the parameter to be trained
     a = np.random.random((2,))
     t = float(input('Learning rate: ')) # learning rate
-    #dr = 1
     epoch = int(input('Epoches: '))
     i = 0
     plt.plot(x_train, a[0]+a[1]*x_train)
@@ -26,15 +26,11 @@ if __name__ == '__main__':
     plt.clf()
 
     while i < epoch:
-        #old_a = a[:] # save data of the old parameter
-
         d0 = 0 # gradient for a_0
         d1 = 0 # gradient for a_1
-        #print('a=',a)
         for j in range(len(x_train)):
             # gradient for a_0:
             dd0 = 2*( a[0] + a[1]*x_train[j] - y_train[j] )
-            #print('dd0=',dd0)
             d0 += dd0
             # gradient for a_1
             d1 += dd0 * x_train[j]
@@ -42,12 +38,19 @@ if __name__ == '__main__':
         a[0] -= t*d0
         a[1] -= t*d1
 
-        #dr = np.linalg.norm(a-old_a) / np.linalg.norm(a)
         if i % 10 == 0 :
             print(a, i)
 
         i += 1
 
-    print('最终参数：', a)
-    plt.plot(x_train, y_train, 'ob', x_train, a[0]+a[1]*x_train)
+    print('Last parameter:', a)
+    x_test = np.array([47, 55])
+    y = a[0] + a[1]*x_train
+    y_test = a[0] + a[1]*x_test
+    plt.plot(x_train, y_train, 'ob', x_train, y, 'b-', x_test, y_test, 'b-')
+    plt.plot(x_test, y_test, 'or')
+    for i in range(2):
+        plt.annotate(s='({}, {})'.format(round(x_test[i], 2), round(y_test[i], 2)),
+                xy=(x_test[i], y_test[i]),
+                xytext=(x_test[i]-2, y_test[i]+2))
     plt.savefig('trained.png')
