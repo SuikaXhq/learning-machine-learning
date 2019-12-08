fe8f1c948c2e9c99d4301368e344792e137b71d7
Xhq <xuhaoqing2013@hotmail.com>
2019-12-09 00:44:31 +0800

    homework mnist ver3

--- source/mnist.py
+++ source/mnist.py
@@ -42,7 +42,7 @@ W_fc1 = weight_variable([7 * 7 * 64, 1024])
 b_fc1 = bias_variable([1024])
 h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-print(h_fc1.shape)
+
 
 keep_prob = tf.placeholder("float")
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
@@ -56,14 +56,25 @@ cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-sess.run(tf.initialize_all_variables())
-for i in range(20000):
-    batch = mnist.train.next_batch(50)
-    if i%100 == 0:
-        train_accuracy = accuracy.eval(feed_dict={
-            x:batch[0], y_: batch[1], keep_prob: 1.0})
-    print("step %d, training accuracy %g"%(i, train_accuracy))
-    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-print("test accuracy %g"%accuracy.eval(feed_dict={
-    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+print('='*100)
+
+print('The shape of the output of layer fc1:')
+print(h_fc1.shape)
+
+print('='*100)
+
+for j in range(1, 5):
+	sess.run(tf.initialize_all_variables())
+	for i in range(2000):
+		batch = mnist.train.next_batch(50)
+		if i%500 == 0:
+			train_accuracy = accuracy.eval(feed_dict={
+				x:batch[0], y_: batch[1], keep_prob: 1.0})
+			print("step %d, training accuracy %g"%(i, train_accuracy))
+		train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: j*0.25})
+	
+	print('='*100)
+	print("dropout %g, test accuracy %g"%(j*0.25, accuracy.eval(feed_dict={
+		x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})))
+	print('='*100)
