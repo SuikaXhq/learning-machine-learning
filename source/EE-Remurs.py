0c4a91d4e12f14a60808040d255e14d58bae5d62
XhqMac <xuhaoqing2013@hotmail.com>
2019-12-11 23:38:00 +0800

    EE-Remurs ver1.0

--- source/EE-Remurs.py
+++ source/EE-Remurs.py
@@ -1,9 +1,12 @@
 import numpy as np
 
 def hyper_para():
-    ratio = float(input('Input tau/gamma ratio (float): '))
-    lam = float(input('Input lambda:'))
-    return ratio, lam
+    '''TODO: path
+    '''
+    # ratio = float(input('Input tau/gamma ratio (float): '))
+    # lam = float(input('Input lambda:'))
+    shape = input('Input the input shape(split by space):')
+    return [int(s) for s in shape.split()]#, path
 
 class EE_Remurs():
     def __init__(self, input_shape):
@@ -11,57 +14,112 @@ class EE_Remurs():
         self.input_shape=input_shape
         self.N = len(input_shape)
 
-    def call(self, X):
+    def __call__(self, X):
         return self.W * X
     
-    def train(self, X_train, y_train, ratio=1.0, lam=0.1, epsl=0.1, ita=None):
+    def train(self, X_train, y_train, ratio=8.0, lam=2, epsl=3.0, ita=None):
         self.ratio = ratio
         self.lam = lam
         self.X_train = X_train
         self.epsl = epsl
-        X_flat = np.reshape(X_train, (X_train.shape[0], -1))
+        X_flat = X_train.reshape((X_train.shape[0], -1))
         P_flat = X_flat.shape[1]
-        W_hat = np.linalg.inv(X_flat.T * X_flat + epsl*np.ones((P_flat, P_flat))) @ X_flat.T @ y_train
+        W_hat = np.linalg.inv(X_flat.T @ X_flat + epsl*np.identity(P_flat)) @ X_flat.T @ y_train
         W_hat = W_hat.reshape(self.input_shape)
         if not ita:
-            ita = [1.0/(self.N+1) for _ in range(self.N)]
+            ita = [1.0/(self.N+1) for _ in range(self.N+1)]
 
         # training
-        Ws = []
-        Ws.append(self.thresholding(W_hat/ita[0], lam))
+        self.Ws = []
+        self.Ws.append(self.thresholding(W_hat/ita[0], lam))
         for i in range(1, self.N+1):
-            W_i = W_hat/ita[i]
+            W_i = (W_hat/ita[i]).reshape((self.input_shape[i-1], -1))
             U, S, V = np.linalg.svd(W_i)
             S = self.thresholding(S, ratio*lam/self.N)
-            Ws.append(U @ S @ V)
-        self.W = np.average(Ws, axis=0)
+            print(S)
+            Sm = np.zeros(W_i.shape)
+            np.fill_diagonal(Sm, S)
+            self.Ws.append((U @ Sm @ V).reshape(self.input_shape))
+        self.W = np.average(self.Ws, axis=0)
+
+    def get_weights(self):
+        return self.Ws, self.W
+
+    def test(self, X_test, y_test):
+        loss = np.linalg.norm(X_test @ self.W - y_test)
+        return loss
     
     def thresholding(self, W, t):
         return np.where(np.fabs(W) > t, np.sign(W)*(np.fabs(W)-t), 0)
 
+class Generator():
+    def __init__(self, shape=(10,10,10,10)):
+        self.shape = shape
+        self.N = len(shape)
+        self.weights = None
     
+    def reshape(self, shape):
+        self.shape = shape
+        self.N = len(shape)
     
-def generate_weight(shape=(10,10,10,10,10), weights=None):
-    N = len(shape)
-    if not weights:
-        weight = (1,)*(N+1)
-    Ws_origin = []
-    Ws_origin.append(np.random.choice([0,1], size=shape, p=[0.9,0.1]))
-    
-    for i in range(N):
-        a = np.random.normal(loc=0, scale=1, size=shape[i]).reshape((-1,1))
-        b = np.random.normal(loc=0, scale=1, size=np.prod(shape)//shape[i]).reshape((-1,1))
-        Ws_origin.append((a @ b.T).reshape(shape))
-    return np.average(Ws_origin, axis=0, weights=weights), Ws_origin
-    
+    def reweights(self, weights):
+        self.weights = weights
 
+    def generate_weight(self):
+        if not self.weights:
+            self.weights = (1,)*(self.N+1)
+        Ws_origin = []
+        Ws_origin.append(np.random.choice([0,1], size=self.shape, p=[0.9,0.1]))
+        
+        for i in range(self.N):
+            a = np.random.normal(loc=0, scale=1, size=self.shape[i]).reshape((-1,1))
+            b = np.random.normal(loc=0, scale=1, size=np.prod(self.shape)//self.shape[i]).reshape((-1,1))
+            Ws_origin.append((a @ b.T).reshape(self.shape))
+        self.Ws = Ws_origin
+        self.W = np.average(Ws_origin, axis=0, weights=self.weights)
+        return self.Ws, self.W
+        
+    def generate_data(self, N):
+        W_ = self.W.reshape((-1))
+    #    print(W_.shape)
+        P = W_.shape[0]
+        X_shape = [N,P]
+        X = np.random.normal(loc=0, scale=10, size=X_shape)
+    #    print(X.shape)
+        bias = np.random.normal(loc=0, scale=1, size=N)
+        y = X@W_ + bias
+        return X.reshape([-1,] + self.shape), y
 
+    '''TODO: test, save and load methods
+    '''
+    def test(self, X_test, y_test):
+        pass
 
-def generate_data(W):
-    bias = np.random.normal()
+    def save(self, path):
+        pass
+
+    def load(self, path):
+        pass
+        
 
 def main():
-    print(generate_weight(shape=(3,4,5), weights=(1,1,1,1)))
+    '''TODO: new or load choices
+    '''
+    shape = hyper_para()
+    generator = Generator(shape)
+    Ws_origin, W = generator.generate_weight()
+#    generator.save(path)
+#    print(W.shape)
+    X_train, y_train = generator.generate_data(10000)
+    X_test, y_test = generator.generate_data(1000)
+    model = EE_Remurs(input_shape=shape)
+    model.train(X_train, y_train)
+    Ws_predict, W_predict = model.get_weights()
+    #print(W)
+    #print(W_predict)
+    print(Ws_predict[0])
+    print(Ws_origin[0])
+    print((W_predict-W)/W)
 
 if __name__ == '__main__':
     main()
