7f23d483db1044350f8effe19e2816b3b4576cb3
Xhq <xuhaoqing2013@hotmail.com>
2019-12-15 07:21:00 +0800

    mnist ver2.0: generating input of liblinear

--- source/mnist.py
+++ source/mnist.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
+import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 sess = tf.InteractiveSession()
 x = tf.placeholder("float", shape=[None, 784])
 y_ = tf.placeholder("float", shape=[None, 10])
+y_label = tf.argmax(y_, axis=1)
 '''
 W = tf.Variable(tf.zeros([784,10]))
 b = tf.Variable(tf.zeros([10]))
@@ -56,25 +58,65 @@ cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-
+'''
 print('='*100)
 
 print('The shape of the output of layer fc1:')
 print(h_fc1.shape)
 
 print('='*100)
-
-for j in range(1, 5):
-	sess.run(tf.initialize_all_variables())
-	for i in range(2000):
-		batch = mnist.train.next_batch(50)
-		if i%500 == 0:
-			train_accuracy = accuracy.eval(feed_dict={
-				x:batch[0], y_: batch[1], keep_prob: 1.0})
-			print("step %d, training accuracy %g"%(i, train_accuracy))
-		train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: j*0.25})
+'''
+sess.run(tf.initialize_all_variables())
+for i in range(2000):
+	batch = mnist.train.next_batch(50)
 	
-	print('='*100)
-	print("dropout %g, test accuracy %g"%(j*0.25, accuracy.eval(feed_dict={
-		x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})))
-	print('='*100)
+	if i%500 == 0:
+		train_accuracy = accuracy.eval(feed_dict={
+			x:batch[0], y_: batch[1], keep_prob: 1.0})
+		print("step %d, training accuracy %g"%(i, train_accuracy))
+	train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.75})
+
+print('='*100)
+print("dropout %g, test accuracy %g"%(0.75, accuracy.eval(feed_dict={
+	x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})))
+print('='*100)
+
+# generate selected features of mnist
+x_train = mnist.train.images[:-15000]
+y_train = mnist.train.labels[:-15000]
+x_train_m = sess.run(h_pool2_flat, feed_dict={x: x_train, y_: y_train})
+y_train_m = tf.reshape(tf.argmax(y_train, axis=1), shape=[-1, 1])
+#x_train_m = x_train_m.eval()
+y_train_m = y_train_m.eval()
+train_data = np.concatenate((y_train_m, x_train_m), axis=1)
+flags = train_data!=0
+with open('mnist.select', mode='w') as f:
+	for i in range(train_data.shape[0]):
+		f.write(str(int(train_data[i][0])))
+		f.write(' ')
+		j=0
+		for flag in flags[i][1:]:
+			if flag:
+				f.write('{}:{} '.format(j+1, train_data[i][j+1]))
+			j+=1
+		f.write('\n')
+
+x_test = mnist.test.images
+y_test = mnist.test.labels
+x_test_m = sess.run(h_pool2_flat, feed_dict={x: x_test, y_: y_test})
+y_test_m = tf.reshape(tf.argmax(y_test, axis=1), shape=[-1, 1])
+#x_test_m = x_test_m.eval()
+y_test_m = y_test_m.eval()
+test_data = np.concatenate((y_test_m, x_test_m), axis=1)
+flags = test_data!=0
+with open('mnist.select.t', mode='w') as f:
+	for i in range(test_data.shape[0]):
+		f.write(str(int(test_data[i][0])))
+		f.write(' ')
+		j=0
+		for flag in flags[i][1:]:
+			if flag:
+				f.write('{}:{} '.format(j+1, test_data[i][j+1]))
+			j+=1
+		f.write('\n')
+	
