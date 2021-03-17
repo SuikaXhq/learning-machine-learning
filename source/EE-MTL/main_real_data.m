dfb701e5503c8971aa85f2b4061ee378d737fa64
Xhq <xuhaoqing2013@hotmail.com>
2021-03-17 21:44:42 +0800

    real exp & some drawings

--- source/EE-MTL/main_real_data.m
+++ source/EE-MTL/main_real_data.m
@@ -1,6 +1,7 @@
 clear;
 load('climate.mat');
-for nu=[0.02, 0.05, 0.1]
-    [beta, alpha, theta, subgroup, timecost] = dishes(X, Z, Y);
+for nu=[0.6 0.7 0.8 0.9 0.95]
+    [beta, alpha, theta, subgroup, timecost] = dishes(X, Z, Y, nu);
     save(sprintf('results/climate_result_nu%.2f.mat',nu), 'beta', 'alpha', 'theta', 'subgroup', 'timecost');
+    output_real_exp;
 end
